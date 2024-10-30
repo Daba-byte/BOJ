ea97b2ae4d840c274af32198d4f32f5da15c5b0a
DABA <dabin12648@gmail.com>
2024-10-30 13:03:27 +0900

    1030

---
+++ 알고리즘하는 멋진 우리/241031/아기_상어.py
@@ -0,0 +1,12 @@
+def find_fish(N, ocean):
+    pass 
+
+
+def baby_shark(N, ocean):
+    pass
+
+N = int(input())
+ocean = [list(int, input().split()) for _ in range(N)]
+
+result = baby_shark(N, ocean)
+print(result)
